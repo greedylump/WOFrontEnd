fc8bdc1536c558b2a5549248feacc320e669b660
Anthony <greedylumpsoftware@gmail.com>
2017-04-08 13:53:54 -0500

    Moving stuff around, trying to comsolidate an outside project into main WOFrontend

---
+++ Exercise/WorkOut/Exercise.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkOutClass.OCExtensions;
+
+namespace WorkOutClass
+{
+    
+    public class Exercise:INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+
+            }
+            
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+        private ObservableCollection<int> sets;
+        public ObservableCollection<int> Sets
+        {
+            get
+            {
+                return sets;
+            }
+            set
+            {
+                sets = value;
+                RaisePropertyChanged("Sets");
+            }
+        }
+
+        public Exercise()
+        {
+            sets = new ObservableCollection<int>();
+        }
+
+        public Exercise(string name)
+        {
+            this.name = name;
+            sets = new ObservableCollection<int>();
+        }
+
+        public Exercise(string name, IEnumerable<int> list)
+        {
+            this.name = name;
+            sets = list.ToObservableCollection();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+
+    }
+}
---
+++ Exercise/WorkOut/WorkOutRepository.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using WOFrontEnd.OCExtensions;
+
+namespace WorkOutClass
+{
+    public class WorkOutRepository
+    {
+      
+        public ObservableCollection<WorkOut> allWorkOuts { get; set; }
+
+        public WorkOut WorkOutByDate(string s)
+        {
+            DateTime date = WorkOut.stringToDate(s);
+
+            var wo = from workKvp in allWorkOuts
+                      where workKvp.Date == date
+                      select workKvp;
+            return wo.ToList()[0];
+        }
+
+        public WorkOutRepository()
+        {
+            allWorkOuts = new ObservableCollection<WorkOut>();
+            
+        }
+        /// <summary>
+        /// Adds workout to Observable Collection
+        /// </summary>
+        /// <param name="work"></param>
+        public void AddToCollection(WorkOut work)
+        {
+            allWorkOuts.Add( work);
+        }
+        
+        /// <summary>
+        /// Reorders the collection of workouts, ordered from earliest to latest
+        /// </summary>
+        /// <returns></returns>
+        public void ToOrderedObservableCollection()
+        {
+            allWorkOuts.Sort();
+        }
+
+        
+    }
+}
