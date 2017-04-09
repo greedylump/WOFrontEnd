0f9cebbf9617ec041cf5b9d0120927b7d4899327
Anthony <greedylumpsoftware@gmail.com>
2017-04-09 09:35:11 -0500

    SMall fix

--- WOFrontEnd/Services/WorkOutDataService.cs
+++ WOFrontEnd/Services/WorkOutDataService.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkOutClass;
+
 using Newtonsoft.Json;
 using System.ComponentModel;
+using WorkOutClass;
 
 namespace WOFrontEnd.Services
 {/// <summary>
