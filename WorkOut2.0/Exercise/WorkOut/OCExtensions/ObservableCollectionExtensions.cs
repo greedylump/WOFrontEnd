656564bb007a8812bb8b126f21c6e89476dec37e
Anthony <greedylumpsoftware@gmail.com>
2017-04-09 16:36:52 -0500

    .

---
+++ WorkOut2.0/Exercise/WorkOut/OCExtensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOFrontEnd.OCExtensions
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void Sort<T>(this ObservableCollection<T> oc)
+        {
+            List<T> ocList = oc.ToList();
+            oc.Clear();
+            ocList.Sort();
+            foreach(var workout in ocList)
+            {
+                oc.Add(workout);
+            }
+        }
+
+
+    }
+}
